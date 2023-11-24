54fdbc8973e613681fd9e62823411a9b067caf39
andyphancode <andyphanpxh@gmail.com>
2023-11-23 18:31:26 -0800

    created profile component

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import PromptCard from './PromptCard';
 
-const Profile = () => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className='w-full'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{name} Profile</span>
+      </h1>
+    </section>
   )
 }
 
