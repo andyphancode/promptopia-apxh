0cf9b08aab6da3bdb927f07717d8043a2dd1f106
andyphancode <andyphanpxh@gmail.com>
2023-11-24 03:01:13 -0800

    created profile page route for non-matching profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,6 +9,7 @@ import Profile from '@components/Profile';
 
 const MyProfile = () => {
     const { data: session } = useSession();
+    const router = useRouter();
 
     const [posts, setPosts]  = useState([]);
     useEffect(() => {
@@ -22,12 +23,26 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts();
       }, [])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+      if(hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE',
+          })
+
+          const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+          setPosts(filteredPosts);
+        } catch (err) {
+          console.log(err);
+        }
+      }
     }
 
   return (
