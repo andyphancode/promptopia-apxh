f0f1fb2a69916dfa469cf9d81d801ca4a480fc62
andyphancode <andyphanpxh@gmail.com>
2023-11-24 16:29:31 -0800

    Update route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,11 +5,12 @@ export const GET = async (req) => {
 
     try {
         await connectToDB();
-        console.log("Posts are being fetched...")
+        console.log("Posts are being fetched...");
         const prompts = await Prompt.find({}).populate('creator');
-        console.log("Posts have been fetched,", prompts)
+        console.log("Posts have been fetched,", prompts);
         return new Response(JSON.stringify(prompts), {status: 200} )
     } catch (error) {
+        console.log(error);
         return new Response("Failed to fetch all prompts", {status: 500})
     }
 }
